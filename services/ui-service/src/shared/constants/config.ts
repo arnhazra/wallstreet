47d583d59542c30f4fd26822d243409fcfa7beb0
arnab <arnhazra@gmail.com>
2026-03-14 12:37:39 +0530

    Publish App

--- services/ui-service/src/shared/constants/config.ts
+++ services/ui-service/src/shared/constants/config.ts
@@ -3,7 +3,7 @@ export const GOOGLE_OAUTH_CLIENT_ID =
   "211974992845-eas8uh5t2ki6jft4tajq57ppr8skseau.apps.googleusercontent.com"
 
 const CORE_SERVICE_URL: Partial<Record<typeof process.env.NODE_ENV, string>> = {
-  production: "https://core-service-wallstreet.azurewebsites.net",
+  production: `https://core-service-${PLATFORM_NAME.toLowerCase()}.azurewebsites.net`,
   development: "http://localhost:8080",
 }
 
